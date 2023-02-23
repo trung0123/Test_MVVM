3116a39152e48f0f37258fefa841dea0610198a4
nesv005 <nesv005@gmail.com>
2023-02-23 15:31:48 +0700

    Upgrade version

--- app/src/main/java/jilnesta/com/testmvvm/data/dto/recipes/RecipesItem.kt
+++ app/src/main/java/jilnesta/com/testmvvm/data/dto/recipes/RecipesItem.kt
@@ -3,7 +3,7 @@ package jilnesta.com.testmvvm.data.dto.recipes
 import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @JsonClass(generateAdapter = false)
 @Parcelize
